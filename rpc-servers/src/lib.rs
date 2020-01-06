94d75e1af8cbe0f8dfb0d5cfcefcb675550b6848
qinghuan <hashlock@163.com>
2020-01-06 12:19:12 +0800

    rpc基础配置

--- rpc-servers/src/lib.rs
+++ rpc-servers/src/lib.rs
@@ -1,27 +1,11 @@
-// Copyright 2017-2019 Parity Technologies (UK) Ltd.
-// This file is part of Substrate.
-
-// Substrate is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// Substrate is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
-
-//! Substrate RPC servers.
+//! RPC servers.
 
 #![warn(missing_docs)]
 
-use std::io;
 use jsonrpc_core::IoHandlerExtension;
 use log::error;
 use pubsub::PubSubMetadata;
+use std::io;
 
 /// Maximal payload accepted by RPC servers.
 const MAX_PAYLOAD: usize = 15 * 1024 * 1024;
@@ -35,9 +19,7 @@ pub type RpcHandler<T> = pubsub::PubSubHandler<T>;
 pub use self::inner::*;
 
 /// Construct rpc `IoHandler`
-pub fn rpc_handler<M: PubSubMetadata>(
-	extension: impl IoHandlerExtension<M>
-) -> RpcHandler<M> {
+pub fn rpc_handler<M: PubSubMetadata>(extension: impl IoHandlerExtension<M>) -> RpcHandler<M> {
 	let mut io = pubsub::PubSubHandler::default();
 	extension.augment(&mut io);
 
@@ -48,10 +30,12 @@ pub fn rpc_handler<M: PubSubMetadata>(
 		let methods = serde_json::to_value(&methods)
 			.expect("Serialization of Vec<String> is infallible; qed");
 
-		move |_| Ok(serde_json::json!({
-			"version": 1,
-			"methods": methods.clone(),
-		}))
+		move |_| {
+			Ok(serde_json::json!({
+				"version": 1,
+				"methods": methods.clone(),
+			}))
+		}
 	});
 	io
 }
@@ -88,34 +72,42 @@ mod inner {
 	}
 
 	/// Start WS server listening on given address.
-	///
-	/// **Note**: Only available if `not(target_os = "unknown")`.
-	pub fn start_ws<M: pubsub::PubSubMetadata + From<jsonrpc_core::futures::sync::mpsc::Sender<String>>> (
+	pub fn start_ws<
+		M: pubsub::PubSubMetadata + From<jsonrpc_core::futures::sync::mpsc::Sender<String>>,
+	>(
 		addr: &std::net::SocketAddr,
 		max_connections: Option<usize>,
 		cors: Option<&Vec<String>>,
 		io: RpcHandler<M>,
 	) -> io::Result<ws::Server> {
-		ws::ServerBuilder::with_meta_extractor(io, |context: &ws::RequestContext| context.sender().into())
-			.max_payload(MAX_PAYLOAD)
-			.max_connections(max_connections.unwrap_or(WS_MAX_CONNECTIONS))
-			.allowed_origins(map_cors(cors))
-			.allowed_hosts(hosts_filtering(cors.is_some()))
-			.start(addr)
-			.map_err(|err| match err {
-				ws::Error::Io(io) => io,
-				ws::Error::ConnectionClosed => io::ErrorKind::BrokenPipe.into(),
-				e => {
-					error!("{}", e);
-					io::ErrorKind::Other.into()
-				}
-			})
+		ws::ServerBuilder::with_meta_extractor(io, |context: &ws::RequestContext| {
+			context.sender().into()
+		})
+		.max_payload(MAX_PAYLOAD)
+		.max_connections(max_connections.unwrap_or(WS_MAX_CONNECTIONS))
+		.allowed_origins(map_cors(cors))
+		.allowed_hosts(hosts_filtering(cors.is_some()))
+		.start(addr)
+		.map_err(|err| match err {
+			ws::Error::Io(io) => io,
+			ws::Error::ConnectionClosed => io::ErrorKind::BrokenPipe.into(),
+			e => {
+				error!("{}", e);
+				io::ErrorKind::Other.into()
+			}
+		})
 	}
 
 	fn map_cors<T: for<'a> From<&'a str>>(
-		cors: Option<&Vec<String>>
+		cors: Option<&Vec<String>>,
 	) -> http::DomainsValidation<T> {
-		cors.map(|x| x.iter().map(AsRef::as_ref).map(Into::into).collect::<Vec<_>>()).into()
+		cors.map(|x| {
+			x.iter()
+				.map(AsRef::as_ref)
+				.map(Into::into)
+				.collect::<Vec<_>>()
+		})
+		.into()
 	}
 
 	fn hosts_filtering(enable: bool) -> http::DomainsValidation<http::Host> {
@@ -131,5 +123,4 @@ mod inner {
 }
 
 #[cfg(target_os = "unknown")]
-mod inner {
-}
+mod inner {}
